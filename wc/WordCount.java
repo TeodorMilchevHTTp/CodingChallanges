0463a50da41cc0c5b7ab4539947df7794c345c86
Teodor <teodor.v.milchev@gmail.com>
2025-08-11 12:12:12 +0100

    Read bytes from file

---
+++ wc/WordCount.java
@@ -0,0 +1,30 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class WordCount {
+
+    public static void main(String[] args) {
+        if (args.length != 1) {
+            System.err.println("Usage: java WordCount <filename>");
+            System.exit(1);
+        }
+        String filename = args[0];
+        long byteCount = 0;
+
+        try (FileInputStream fis = new FileInputStream(filename)) {
+
+            byte[] buffer = new byte[4096];
+            int bytes;
+
+            while ((bytes = fis.read(buffer)) != -1) {
+                byteCount += bytes;
+            }
+            System.out.println(byteCount);
+
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+            System.exit(1);
+        }
+
+    }
+}
